ef1e75479e22c3c00080300cccaa0ea207712793
Stats_Teacher <Stats.Teacher@avc.edu>
2018-03-10 13:36:37 -0800

    finish mostly

---
+++ Chap_3_WhatsWrong.R
@@ -0,0 +1,16 @@
+library(shiny)
+
+ui <- fluidPage(
+  titlePanel("Add 2"),
+  sidebarLayout(
+    sidebarPanel( sliderInput("x", "Select x", min = 1, max = 50, value = 30) ),
+    mainPanel( textOutput("x_updated") )
+  )
+)
+
+add_2 <- function(x) { x + 2 }
+
+server <- function(input, output) {
+  current_x        <- add_2(input$x)
+  output$x_updated <- renderText({ current_x })
+}
